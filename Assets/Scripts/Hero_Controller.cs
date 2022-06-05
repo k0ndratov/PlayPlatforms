c64eb532fdf1bcb3f4025feb25faa73b8b944471
k0ndratov <93945565+k0ndratov@users.noreply.github.com>
2022-06-05 15:55:15 +0300

    Update Hero_Controller.cs
    Fix multiple jump

--- Assets/Scripts/Hero_Controller.cs
+++ Assets/Scripts/Hero_Controller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Hero_Controller : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Collider2D coll;
+    public LayerMask groundLayer;
 
     [Range(0, 15)]
     public float moveSpeed;
@@ -13,6 +15,7 @@ public class Hero_Controller : MonoBehaviour
 
     private void Awake()
     {
+        coll = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -31,9 +34,14 @@ public class Hero_Controller : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, groundLayer);
     }
 }
